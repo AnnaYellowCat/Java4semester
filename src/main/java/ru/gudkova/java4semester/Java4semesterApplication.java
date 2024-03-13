1b7610a383ebfef4df09ee81903b6e5a9608555f
Anna <anna.gudkova.rus@mail.ru>
2024-03-13 21:36:56 +0400

    initial commit

---
+++ src/main/java/ru/gudkova/java4semester/Java4semesterApplication.java
@@ -0,0 +1,13 @@
+package ru.gudkova.java4semester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java4semesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java4semesterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/gudkova/java4semester/Java4semesterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gudkova.java4semester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java4semesterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
